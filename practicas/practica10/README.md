6a489c35be9a044d98b5bc0626f16c8df6401069
Juanca <juancasan17@gmail.com>
2018-03-16 11:20:20 -0600

    practica10

---
+++ practicas/practica10/README.md
@@ -0,0 +1,6 @@
+0> o < Entrada estandar "stdin"
+1> Salida estandar "stdout"
+2> Salida de errores "stderr"
+">>" Concatena hacia la salida estandar
+... argumentos ilimitados en nuestro macro
+fgetc Regresa el apuntador del caracter leido por una entrada especifica
---
+++ practicas/practica10/input.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PRINTDEBUG(...)                           \
+  do {                                            \
+    char *debug;                                  \
+    debug = getenv("DEBUG");                      \
+    if (debug != NULL && !strcasecmp(debug, "y")) \
+    { fprintf(stderr, __VA_ARGS___);  }           \
+  } while (0)
+
+int main(int argc, char const *argv[]) {
+  int character;
+  while (1) {
+    character = fgetc(stdin);
+    if(feof(stdin)) {
+      break;
+    }
+    fputc(character, stdout);
+    PRINTDEBUG("character = %c\n", character);
+  }
+  return 0;
+}
